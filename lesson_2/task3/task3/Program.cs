209005d3e64dcba0c7d35d2825a2a52f6c2d88bd
Aliaksandr Zakharevich <jonnydukalez@gmail.com>
2020-10-07 16:54:36 +0300

    task_3 done

---
+++ lesson_2/task3/task3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace task3
+{
+    struct RequestItem
+    {
+        string nameProduct;
+        int numderOfItem;
+
+        public RequestItem (string nameProduct, int numberOfItem)
+        {
+            this.nameProduct = nameProduct;
+            this.numderOfItem = numberOfItem;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Item: {nameProduct} \nQuantity: {numderOfItem}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = "Printer";
+            int quantity = 8;
+
+            RequestItem printers = new RequestItem(product, quantity);
+            printers.Print();
+        }
+    }
+}
