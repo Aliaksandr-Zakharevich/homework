d6c87af48d354c8dae61bbf11d9875d66f930c35
Aliaksandr Zakharevich <jonnydukalez@gmail.com>
2020-10-07 16:25:05 +0300

    done 1 task

---
+++ lesson_2/task1/task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace task1
+{
+    //Описать структуру Article, содержащую поля код товара, название товара, цену товара
+    class Program
+    {
+        struct Article
+        {
+            string codeOfProduct;
+            string nameProduct;
+            double priseProduct;
+
+            public Article(string code, string name, double prise)
+            {
+                codeOfProduct = code;
+                nameProduct = name;
+                priseProduct = prise;
+            }
+            public void Print()
+            {
+                Console.WriteLine($"code: {codeOfProduct}, name: {nameProduct}, price: {priseProduct}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            string code = "0fx1456";
+            string name = "Smartphone";
+            double price = 134.632;
+            Article a = new Article(code, name, price);
+            a.Print();
+        }
+    }
+}
