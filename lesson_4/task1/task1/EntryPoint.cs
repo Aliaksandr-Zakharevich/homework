d5066e1cc8ec1c683b8fad8b441d590d94394186
Aliaksandr Zakharevich <jonnydukalez@gmail.com>
2020-11-09 20:42:24 +0300

    create new branch and done task1

---
+++ lesson_4/task1/task1/EntryPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace task1
+{
+    /*
+     Разработать собственный структурный тип данных для хранения целочисленных коэффициентов А и В
+    линейного уравнения A*X + B*Y = 0. В классе реализовать статический метод Parse(), которые принимает 
+    строку со значениями коэффициентов, разделенных запятой или пробелом
+     */
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter linear:");
+            Linear A1 = new Linear(Console.ReadLine());
+            A1.Print();          
+        }
+    }
+}
---
+++ lesson_4/task1/task1/Linear.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task1
+{
+    class Linear
+    {
+        int A;
+        int B;
+
+        public Linear(string value)
+        {
+            string[] _value = Parse(value);
+            if (_value.Length == 2)
+            {
+                A = Convert.ToInt32(_value[0]);
+                B = Convert.ToInt32(_value[1]);
+            }
+            else
+            {
+                A = Convert.ToInt32(_value[0]);
+                B = Convert.ToInt32(_value[2]);
+            }
+        }
+        public static string[] Parse(string userInput)
+        {
+            string[] userValue = userInput.Split(" ,.".ToCharArray());
+            return userValue;
+        }
+        public void Print()
+        {
+            Console.Write($"{A}*X + {B}*Y = 0");
+        }
+    }
+}
